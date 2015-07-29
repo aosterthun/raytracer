41bec5202f62efcddd114e2d26821d43251a9f05
DaCortex <manuelhartmann@outlook.com>
2015-07-29 11:16:46 +0200

    Added method-body to intersect() method of box.cpp

--- framework/box.cpp
+++ framework/box.cpp
@@ -12,9 +12,7 @@ Box::Box(glm::vec3 const& min, glm::vec3 const& max):
 Shape::Shape("untitled_box", Color{0.0}),
 _min{min},
 _max{max}
-{
-	std::cout << "Box construct \n";
-}
+{}
 
 Box::Box(glm::vec3 const& min, glm::vec3 const& max, std::string const& name, Color const& color):
 Shape::Shape(name, color),
@@ -34,9 +32,27 @@ std::ostream& Box::print(std::ostream& os) const
 	return os;
 }
 
-bool Box::intersect(Ray const& ray, float& distance) const
+bool Box::intersect(Ray const& r, float& distance) const
 {
-	throw std::runtime_error("not implemented");
+	double tx1 = (_min.x - r.origin.x)/glm::normalize(r.direction).x;
+	double tx2 = (_max.x - r.origin.x)/glm::normalize(r.direction).x;
+
+	double tnear = std::min(tx1, tx2);
+	double tfar = std::max(tx1, tx2);
+
+	double ty1 = (_min.y - r.origin.y)/glm::normalize(r.direction).y;
+	double ty2 = (_max.y - r.origin.y)/glm::normalize(r.direction).y;
+
+	tnear = std::max(tnear, std::min(ty1, ty2));
+	tfar = std::min(tfar, std::max(ty1, ty2));
+
+	double tz1 = (_min.z - r.origin.z)/glm::normalize(r.direction).z;
+	double tz2 = (_max.z - r.origin.z)/glm::normalize(r.direction).z;
+
+	tnear = std::max(tnear, std::min(tz1, tz2));
+	tfar = std::min(tfar, std::max(tz1, tz2));
+
+	return tfar >= std::max(0.0, tnear);
 }
 
 glm::vec3 Box::min() const
