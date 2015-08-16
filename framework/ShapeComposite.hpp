924b6de70d96cc380d2d2dc031c48463917fe8cb
Arne Osterthun <arne.osterthun@uni-weimar.de>
2015-08-16 14:15:26 +0200

    Added interface for the ShapeComposite

--- framework/ShapeComposite.hpp
+++ framework/ShapeComposite.hpp
@@ -1,11 +1,17 @@
 #ifndef BUW_SHAPECOMPOSITE_HPP
 #define BUW_SHAPECOMPOSITE_HPP
-
-class ShapeComposite
+#include "shape.hpp"
+class ShapeComposite : public Shape
 {
 	public:
 		ShapeComposite();
+		void add(Shape const& shape);
+		void remove(Shape const& shape);
+		bool intersect(Ray const& ray, float& distance) const override;
 		~ShapeComposite();
+
+	private:
+		std::vector<Shape> shapes;
 	
 };
 
