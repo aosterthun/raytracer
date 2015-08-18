87252e1012181b0d57babb51aa22c5c2a9f45edd
DaCortex <manuelhartmann@outlook.com>
2015-08-18 12:51:04 +0200

    Added render() raycast() trace() and shade()

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -19,21 +19,48 @@ Renderer::Renderer(unsigned w, unsigned h, std::string const& file)
 
 void Renderer::render()
 {
-  const std::size_t checkersize = 20;
+	/*
+	//NOTE: MIGHT BE OUTSIDE
+	SDFloader loader{};
+	loader.readSdf(filename_);
 
-  for (unsigned y = 0; y < height_; ++y) {
-    for (unsigned x = 0; x < width_; ++x) {
-      Pixel p(x,y);
-      if ( ((x/checkersize)%2) != ((y/checkersize)%2)) {
-        p.color = Color(0.0, 1.0, float(x)/height_);
-      } else {
-        p.color = Color(1.0, 0.0, float(y)/width_);
-      }
+	//retrieving the scene from the loader
+	Scene scene = loader.scene();
+	//////////////////////////////////////////
 
-      write(p);
-    }
-  }
-  ppm_.save(filename_);
+	raycast(colorbuffer_, scene.camera);
+
+	Pixel p(300,300);
+	p.color = Color(1.0, 1.0, 1.0);
+	write(p);
+
+	ppm_.save(filename_);
+	
+	*/
+}
+
+void Renderer::raycast(std::vector<Color> &colorbuffer, Camera &camera)
+{
+	/*
+	float distance = camera.getDistance(width_);
+	for(auto pixel : colorbuffer)
+	{
+		pixel = trace(camera.getEyeRay( (pixel.x)-960, (pixel.y)-960, distance));
+	}
+	*/
+}
+
+
+Color Renderer::trace(Ray r)
+{
+
+	return ;
+}
+
+Color Renderer::shade(std::shared_ptr<Shape> s, Ray r, double t)
+{
+
+	return ;
 }
 
 void Renderer::write(Pixel const& p)
@@ -51,3 +78,26 @@ void Renderer::write(Pixel const& p)
 
   ppm_.write(p);
 }
+
+/*
+const std::size_t checkersize = 20;
+
+for (unsigned y = 0; y < height_; ++y)
+{
+	for (unsigned x = 0; x < width_; ++x)
+	{
+		Pixel p(x,y);
+		if ( ((x/checkersize)%2) != ((y/checkersize)%2))
+		{
+		p.color = Color(0.0, 1.0, float(x)/height_);
+		}
+		else
+		{
+		p.color = Color(1.0, 0.0, float(y)/width_);
+		}
+
+		write(p);
+	}
+}
+ppm_.save(filename_);
+*/
--- framework/renderer.hpp
+++ framework/renderer.hpp
@@ -13,28 +13,42 @@
 #include "color.hpp"
 #include "pixel.hpp"
 #include "ppmwriter.hpp"
+#include "sdfloader.hpp"
+#include "ray.hpp"
+#include "shape.hpp"
+#include "camera.hpp"
+
+#include <memory>
 #include <string>
 #include <glm/glm.hpp>
 
 class Renderer
 {
 public:
-  Renderer(unsigned w, unsigned h, std::string const& file);
 
-  void render();
-  void write(Pixel const& p);
+	Renderer(unsigned w, unsigned h, std::string const& file);
 
-  inline std::vector<Color> const& colorbuffer() const
-  {
-    return colorbuffer_;
-  }
+	void render();
+	void write(Pixel const& p);
+
+	void raycast(std::vector<Color> &colorbuffer, Camera &camera);
+
+	inline std::vector<Color> const& colorbuffer() const
+	{
+		return colorbuffer_;
+	}
 
 private:
-  unsigned width_;
-  unsigned height_;
-  std::vector<Color> colorbuffer_;
-  std::string filename_;
-  PpmWriter ppm_;
+
+	unsigned width_;
+	unsigned height_;
+	std::vector<Color> colorbuffer_;
+	std::string filename_;
+	PpmWriter ppm_;
+
+	Color trace(Ray r);
+
+	Color shade(std::shared_ptr<Shape> s, Ray r, double t);
 };
 
 #endif // #ifndef BUW_RENDERER_HPP
