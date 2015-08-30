a5e58f51a7a7761d90b5f9d7a3144b755368c270
DaCortex <manuelhartmann@outlook.com>
2015-08-30 15:32:19 +0200

    added _ray as member variable

--- framework/optionalhit.hpp
+++ framework/optionalhit.hpp
@@ -14,29 +14,44 @@ struct OptionalHit
 	OptionalHit() :
 	_hit{},
 	_shape{},
+	_depth{0},
 	_t{0.0}
 	{}
 	
 	OptionalHit(bool hit, std::shared_ptr<Shape> shape, double t) :
 	_hit{hit},
+	_depth{0},
 	_shape{shape},
 	_t{t}
 	{}
 
-	OptionalHit(bool hit, std::shared_ptr<Shape> shape, double t, glm::vec3 const& intersect, glm::vec3 const& normal) :
+	OptionalHit(bool hit, std::shared_ptr<Shape> shape, double t, glm::vec3 const& intersect, Ray const& ray, glm::vec3 const& normal) :
 	_hit{hit},
+	_depth{0},
 	_shape{shape},
 	_t{t},
 	_intersect{intersect},
+	_ray{ray},
 	_normal{normal}
 	{}
 
 	//variables
+
+	//wether the object was hit or not
 	bool _hit;
+	//recursion depth
 	int _depth;
+	//the object that was hit
 	std::shared_ptr<Shape> _shape;
+	//distance between _ray.origin and _intersect
 	double _t;
+
+	//intersecting point
 	glm::vec3 _intersect;
+
+	//intersecting ray
+	Ray _ray;
+
 	glm::vec3 _normal;
 };
 
